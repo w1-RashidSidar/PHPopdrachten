16706ada61f21b4230a4e33a7149fd847189b936
Sidar Rashid <sidar.rashid@edu-kw1c.nl>
2020-02-17 16:15:11 +0100

    commit all files

--- Hoofdstuk2/opdracht2.2.php
+++ Hoofdstuk2/opdracht2.2.php
@@ -6,21 +6,21 @@
  * File: index.php
  */
 
-$text1 = 'Hallo';
-$text2 = 'een makkelijke taal';
-$text3 = "toch zo'n makkelijke taal";
-$text4 = 'wat is';
-$text5 = 'PHP';
-$text6 = 'Nooit gedacht dat';
-$text7 = 'De installatie is best ingewikkeld';
-$text8 = 'Fijn';
-$text9 = '?';
-$text10 = '.';
-$text11 = ',';
+$text1 = ' Hallo ';
+$text2 = ' een makkelijke taal ';
+$text3 = " toch zo'n makkelijke taal ";
+$text4 = ' wat is ';
+$text5 = ' PHP ';
+$text6 = ' Nooit gedacht dat ';
+$text7 = ' De installatie is best ingewikkeld ';
+$text8 = ' Fijn ';
+$text9 = ' ? ';
+$text10 = '. ';
+$text11 = ', ';
 $text12 = '<br>';
-$text13 = 'is';
-$text14 = 'Vind je niet';
-$text15 = 'toch';
+$text13 = ' is ';
+$text14 = 'Vind je niet ';
+$text15 = ' toch ';
 
 ?>
 
@@ -44,31 +44,29 @@ $text15 = 'toch';
     </head>
 
     <body>
+    <h2>
+        Taak 2
+    </h2>
+    <?php
+echo               "<p>" .
+                        $text1 . $text11 . $text4 . $text5 . $text15 . $text2.$text10 . $text12.
+                        $text7.$text10 .$text8 .$text15.$text9 .$text12.
+                        $text6. $text5 .$text3. $text13.$text10.
+                   "</p>"
+
+    ?>
+    <br>
+    <h2>
+        Taak 3
+    </h2>
     <?php
     echo
-    "<h2>Taak 2</h2>
-                
-                    <p>
-                        $text1$text11 $text4 $text5 $text15 $text2$text10 $text12
-                        $text7$text10 $text8 $text15$text9 $text12
-                        $text6 $text5 $text3 $text13$text10
-                    </p>
-                    $text12"
+                    "<p>".
+                        $text1 ." " .$text11 . $text12.
+                        $text8 . $text15 . "dat" . $text5 . "zo'n makkelijke taal" .$text13.$text10. $text12.
+                        $text7.$text10 . $text14 . $text9 .
+                    "</p>"
     ?>
-
-    <?php
-    echo
-    "<h2>
-                           Taak 3
-                    </h2>
-                    
-                    <p>
-                        $text1$text11 $text12
-                        $text8 $text15 dat $text5 zo'n makkelijke taal $text13$text10 $text12
-                        $text7$text10 $text14$text9
-                    </p>"
-    ?>
-
     </body>
 </html>
 
