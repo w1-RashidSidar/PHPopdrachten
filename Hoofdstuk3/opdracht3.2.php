93e54baac1be596153910ae88098df0623596be8
Sidar Rashid <sidar.rashid@edu-kw1c.nl>
2020-03-02 16:21:36 +0100

    commit all files

--- Hoofdstuk3/opdracht3.2.php
+++ Hoofdstuk3/opdracht3.2.php
@@ -1,1 +1,57 @@
 <?php
+include "../includes/Header.php";
+include "../Includes/Menu.php";
+?>
+    <html lang="en">
+    <body>
+        <main id="wrapper">
+            <h2>Uitwerkingen</h2>
+        </main>
+
+
+<?php
+    $driveOn = true;
+    $trafficLightColor = "Green";
+    $AmbulanceComing = false;
+
+    if ($trafficLightColor == "Green" && $AmbulanceComing == false)
+    {
+        $driveOn = true;
+    }
+    elseif ($trafficLightColor == "Green" && $AmbulanceComing == true)
+    {
+       $driveOn = false;
+    }
+    elseif ($trafficLightColor == "Orange" && $AmbulanceComing == true)
+    {
+       $driveOn = false;
+    }
+    elseif ($trafficLightColor == "Orange" && $AmbulanceComing == false)
+    {
+       $driveOn = false;
+    }
+    elseif ($trafficLightColor == "Red" && $AmbulanceComing == true)
+    {
+       $driveOn = false;
+    }
+    elseif ($trafficLightColor == "Red" && $AmbulanceComing == false)
+    {
+       $driveOn = false;
+    }
+
+    if ($driveOn == true)
+    {
+        echo "<p> u mag doorrijden </p>";
+    }
+    elseif ($driveOn == false)
+    {
+        echo "<p> u moet stoppen </p>";
+    }
+
+?>
+
+
+        </body>
+    </html>
+
+
